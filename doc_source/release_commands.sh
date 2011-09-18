daf63b13145df0175eb12ab420a4dc43df70072b
Antonio Gonzalez <antgonza@gmail.com>
2011-09-18 14:15:21 +0000

    Changes due to moving the file from / to doc_source

--- doc_source/release_commands.sh
+++ doc_source/release_commands.sh
@@ -1,13 +1,13 @@
 version=0.9
 
 echo "Creating ~/sitepainter_$version.zip"
-svn export . ~/sitepainter_$version
+svn export ../ ~/sitepainter_$version
 zip -r ~/sitepainter_$version.zip ~/sitepainter_$version
 
 echo "Creating ~/SitePainter_example_files.zip"
-svn export files ~/SitePainter_example_files
+svn export ../files ~/SitePainter_example_files
 zip -r ~/SitePainter_example_files.zip ~/SitePainter_example_files
 
 
 echo "Do not forget to change: doc_source/install/index.rst to point to the new release file"
-echo "Upload ~/sitepainter_$version.zip &~/SitePainter_example_files.zip to files/"
+echo "Upload ~/sitepainter_$version.zip & ~/SitePainter_example_files.zip to files/"
