949ae419ed0fbd458c19984fd2694a55ff7905f6
Antonio Gonzalez <antgonza@gmail.com>
2011-09-18 17:40:41 +0000

    Fixing some files for release

--- doc_source/release_commands.sh
+++ doc_source/release_commands.sh
@@ -5,9 +5,11 @@ svn export ../ ~/sitepainter_$version
 zip -r ~/sitepainter_$version.zip ~/sitepainter_$version
 
 echo "Creating ~/SitePainter_example_files.zip"
-svn export ../files ~/SitePainter_example_files
+svn export files ~/SitePainter_example_files
 zip -r ~/SitePainter_example_files.zip ~/SitePainter_example_files
 
-
+echo ""
+echo ""
+echo "*****************************"
 echo "Do not forget to change: doc_source/install/index.rst to point to the new release file"
 echo "Upload ~/sitepainter_$version.zip & ~/SitePainter_example_files.zip to files/"
