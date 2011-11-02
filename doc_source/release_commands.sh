99c98c38f81113ebc9629a44929da943c95c91e4
Antonio Gonzalez <antgonza@gmail.com>
2011-11-02 16:10:17 +0000

    Actual version used for release

--- doc_source/release_commands.sh
+++ doc_source/release_commands.sh
@@ -2,11 +2,13 @@ version=0.9
 
 echo "Creating ~/sitepainter_$version.zip"
 svn export ../ ~/sitepainter_$version
-zip -r ~/sitepainter_$version.zip ~/sitepainter_$version
 
 echo "Creating ~/SitePainter_example_files.zip"
 svn export files ~/SitePainter_example_files
-zip -r ~/SitePainter_example_files.zip ~/SitePainter_example_files
+
+cd ~/
+zip -r sitepainter_$version.zip sitepainter_$version
+zip -r SitePainter_example_files.zip SitePainter_example_files
 
 echo ""
 echo ""
